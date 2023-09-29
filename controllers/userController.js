3d98d1e41acbbe7af37a47dde9a979cf42c34c6b
MickFrog <107855646+MickFrog@users.noreply.github.com>
2023-09-29 21:03:32 +0300

    add user sign-up + fix passport authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,14 +1,18 @@
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 const Message = require("../models/message");
 
 exports.index_get = asyncHandler(function (req, res, next) {
   // if no logged in user
-  if (!req.session.user) {
+  if (!req.user) {
     res.redirect("/login");
     return;
   }
+
+  // TODO: Fetch user messages
   res.render("index", { title: "Members Forum", messages: [] });
 });
 
@@ -26,6 +30,55 @@ exports.login_user_post = [
     .trim()
     .isLength({ min: 3 })
     .escape(),
+  body(
+    "password",
+    "Password must be filled and have more than 4 characters"
+  ).isLength({ min: 4 }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.render("login", {
+        title: "User login",
+        validationErrors: errors,
+      });
+    }
+
+    const inUser = await User.findOne({ username: req.body.username });
+
+    if (!inUser) {
+      return res.render("login", {
+        title: "User login",
+        error: "Username is invalid",
+      });
+    }
+
+    const match = await bcrypt.compare(req.body.password, inUser.password);
+    if (!match) {
+      return res.render("login", {
+        title: "User login",
+        error: "Password is invalid",
+      });
+    }
+
+    // successful authentication
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+    })(req, res, next); // to ensure calling of the passport middleware
+  }),
+];
+
+exports.create_user_get = asyncHandler(function (req, res, next) {
+  res.render("sign-up", { title: "User sign-up" });
+});
+
+exports.create_user_post = [
+  body("username", "Username must be filled and with more than 3 characters")
+    .trim()
+    .isLength({ min: 3 })
+    .escape(),
   body("password", "Password must be filled and have more than 4 characters")
     .trim()
     .isLength({ min: 4 })
@@ -35,38 +88,35 @@ exports.login_user_post = [
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.render("/login", {
-        title: "User login",
+      return res.render("/sign-up", {
+        title: "User sign-up",
         validationErrors: errors,
       });
     }
 
-    const inUser = await User.findOne({ username: req.body.username });
+    try {
+      bcrypt.hash(req.body.password, 8, async (err, hashedPass) => {
+        if (err) {
+          err = new Error("Error hashing the password");
+          return;
+        }
 
-    if (!inUser) {
-      return res.render("/login", {
-        title: "User login",
-        error: "Username is invalid",
-      });
-    }
+        // create new user with hashed password
+        const newUser = new User({
+          username: req.body.username,
+          password: hashedPass,
+        });
 
-    const match = await bcrypt.compare(password, inUser.password);
-    if (!match) {
-      return res.render("/login", {
-        title: "User login",
-        error: "Password is invalid",
+        await newUser.save();
       });
+    } catch (err) {
+      return next(err);
     }
 
     // successful authentication
-    res.redirect("/");
+    passport.authenticate("local", {
+      successRedirect: "/",
+      failureRedirect: "/login",
+    })(req, res, next); // to ensure calling of the passport middleware
   }),
 ];
-
-exports.create_user_get = asyncHandler(function (req, res, next) {
-  res.render("sign-up", { title: "User sign-up" });
-});
-
-exports.create_user_post = asyncHandler(function (req, res, next) {
-  res.send("NOT IMPLEMENTED: To add new user to db");
-});
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const Message = require("../models/message");
 const userController = require("../controllers/userController");
 
 /* GET home page. */
